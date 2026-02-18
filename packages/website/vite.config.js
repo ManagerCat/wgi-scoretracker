dc5c5e0a321b2f3624791f2b158547661d26b886
ManagerCat <67668325+ManagerCat@users.noreply.github.com>
2026-02-18 00:08:26 -0800

    chore: update vite config for development

--- packages/website/vite.config.js
+++ packages/website/vite.config.js
@@ -14,5 +14,11 @@ export default defineConfig({
   server: {
     port: 3000,
     open: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
   },
 });
