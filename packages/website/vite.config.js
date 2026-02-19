c6ca1a1e18d15c3cdd13c10256f6aa28974bd2bc
ManagerCat <67668325+ManagerCat@users.noreply.github.com>
2026-02-18 21:46:23 -0800

    fix(website): useless

--- packages/website/vite.config.js
+++ packages/website/vite.config.js
@@ -14,11 +14,5 @@ export default defineConfig({
   server: {
     port: 3000,
     open: false,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-    },
   },
 });
